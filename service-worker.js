6f10490ebbb999144fb81c59b705e19dec4516d4
Andrii Suboch <andrii.suboch@gmail.com>
2020-08-31 16:29:38 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.95d0b1f67d0fdc027687fdcaf703eb6c.js"
 );
 
 self.addEventListener('message', (event) => {
